1b77724fa6fa58626b4ab64fb2fa8406e6bf18e5
Isaac Guadanholi <zaac.silvestre@gmail.com>
2025-03-27 15:42:33 -0300

    adicionando os metodos get,delete e login

--- src/main/java/com/agendador/usuario/business/UsuarioService.java
+++ src/main/java/com/agendador/usuario/business/UsuarioService.java
@@ -3,20 +3,44 @@ package com.agendador.usuario.business;
 import com.agendador.usuario.business.converter.UsuarioConverter;
 import com.agendador.usuario.business.dto.UsuarioDTO;
 import com.agendador.usuario.infrastructure.entity.Usuario;
+import com.agendador.usuario.infrastructure.exceptions.ConflictException;
+import com.agendador.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.agendador.usuario.infrastructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+
 public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
+    private final PasswordEncoder passwordEncoder;
 
 public UsuarioDTO salvarUsuario (UsuarioDTO usuarioDTO){
+  emailExiste(usuarioDTO.getEmail());
+    usuarioDTO.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
     Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
     return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
 }
 
+    public void emailExiste(String email) {
+        boolean existe = verificaEmailExistente(email);
+        if (existe) {
+            throw new ConflictException("Email Já cadastrado: " + email);
+        }
+    }
 
+    private boolean verificaEmailExistente(String email) {
+        return usuarioRepository.existsByEmail(email);}
+
+    public Usuario buscarPorEmail(String email) {
+        return usuarioRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("Email não encontrado" + email));
+    }
+
+    public void deletaUsuarioPorEmail(String email) {
+        usuarioRepository.deleteByEmail(email);
+
+    }
 }
